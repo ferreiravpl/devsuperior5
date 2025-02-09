98b68c37ac3d626b95fd7369fa61af6093483c24
Lucas Ferreira <ferreiravpl@gmail.com>
2025-02-09 17:43:50 -0300

    Criando endpoint para retornar categorias

---
+++ src/main/java/com/devsuperior/dscommerce/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.dto.CategoryDTO;
+import com.devsuperior.dscommerce.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        return repository.findAll().stream().map(CategoryDTO::new).toList();
+    }
+}
